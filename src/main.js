7bc67b9d0c05a4915c0a3018defd730dfbb27951
shibashui <75465196+shibashui@users.noreply.github.com>
2023-08-10 07:20:24 +0000

    Deploying to vue2_h5 from @ shibashui/project-cece@5de7cdb310d879624382e1f6a02aa3df6e8e0e7b 🚀

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import './assets/main.css'
-import { setStorage, getStorage } from '@/utils/setStore'
-import { createApp } from 'vue'
-import App from './App.vue'
-import { Dialog } from 'vant'
-const app = createApp(App)
-app.config.globalProperties.$setStorage = setStorage
-app.mount('#app')
-app.use(Dialog)
